eb34953e1ca41049ddad3638212c3df67fcdf8ad
Elvin Mendez <[elvinmanuel005@gmail.com]>
2025-01-24 21:45:26 -0400

    Actualizad

---
+++ OOP CalcularSalario/CalcularSalario/Clases/DocentesFijos.cs
@@ -0,0 +1,24 @@
+﻿
+namespace CalcularSalario.Clases
+{
+    public class DocentesFijos : Empleado
+    {
+        public decimal Bonificaciones { get; set; }
+        public DocentesFijos(string nombre, string apellido, string sexo, decimal salario,
+            decimal bonificaciones) : base(nombre, apellido, sexo, salario)
+        {
+            Bonificaciones = bonificaciones;
+        }
+
+        public override decimal CalcularSalarioMensual()
+        {
+            if (Salario >= 5000)
+            {
+                return Salario + Bonificaciones;
+            } else {
+                return (Salario + Bonificaciones) / 2;
+            }
+        }
+        public override string ToString() => $" Nombre: {Nombre} \n Apellido: {Apellido} \n Sexo: {Sexo}";
+    }
+}
