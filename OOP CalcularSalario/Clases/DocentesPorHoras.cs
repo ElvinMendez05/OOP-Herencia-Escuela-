b625b49968a3ae853576a71fe42e9a28d9b87f2a
Elvin Mendez <[elvinmanuel005@gmail.com]>
2025-01-24 21:45:26 -0400

    Actualizad

---
+++ OOP CalcularSalario/Clases/DocentesPorHoras.cs
@@ -0,0 +1,23 @@
+﻿
+namespace CalcularSalario.Clases
+{
+    public class DocentesPorHoras : Empleado
+    {
+        public decimal Horas { get; set; }
+        public decimal Tarifa { get; set; }
+        public DocentesPorHoras(string nombre, string apellido, string sexo, 
+            decimal salario, decimal horas, decimal tarifa):base(nombre, apellido, sexo, salario) 
+        { 
+           Horas = horas;
+           Tarifa = tarifa;
+        }
+        public override decimal CalcularSalarioMensual()
+        {
+            Salario = Horas * Tarifa;
+            return Salario;
+
+        }
+        public override string ToString() => $" Nombre: {Nombre} \n Apellido: {Apellido} \n Sexo: {Sexo}";
+
+    }
+}
